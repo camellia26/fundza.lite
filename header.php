6ff49ffdcda831c819afe1dff05be36cc2348dcb
Yoshiaki NAGASAWA <ykk.26camellia@gmail.com>
2017-09-01 09:26:24 +0200

    minor change

--- header.php
+++ header.php
@@ -43,10 +43,10 @@
             <label id="menu-background" for="menu-cb"></label>
             <div id="ham-menu">
                 <div class="hamElements"><a href="<?php echo home_url(); ?>">HOME</a></div>
-                <div class="hamElements"><a href="<?php echo home_url('/collections') ?>">COLLECTIONS</a></div>
+                <div class="hamElements"><a href="<?php echo home_url('/collections') ?>">COLLECTION</a></div>
                 <div class="hamElements"><a href="<?php echo home_url('/about') ?>">ABOUT</a></div>
-                <div class="hamElements"><a href="<?php echo home_url('/terms') ?>">T&amp;Cs</a></div>
-                <div class="hamElements"><a href="<?php echo home_url('/contact') ?>">CONTACT</a></div>
+                <div class="hamElements"><a href="<?php echo home_url('/terms') ?>">T &amp; Cs</a></div>
+                <div class="hamElements"><a href="<?php echo home_url('/contact') ?>">CONTACT US</a></div>
             </div>
             <div style="clear:both"></div>
         </div>
