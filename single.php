954695c792d9491dc31c1d077a40ba7fdd78a824
Yoshiaki NAGASAWA <ykk.26camellia@gmail.com>
2017-09-06 12:55:44 +0200

    minor change

--- single.php
+++ single.php
@@ -82,15 +82,15 @@
                  </div>
             <?php endwhile;endif; ?>
             <div id="pageTopLink"><a href="#">&#x25B2;Back to top</a></div>
-
-            <!-- top button -->
-            <p class="pagetop">
-                <a href="#">
-                    <img src="<?php echo get_template_directory_uri(); ?>/images/topButton.png" class="topButton" alt="Back to top">
-                </a>
-            </p>
         </div><!--content-->
 
     </div>
+
+    <!-- top button -->
+    <p class="pagetop">
+        <a href="#">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/topButton.png" class="topButton" alt="Back to top">
+        </a>
+    </p>
 
 <?php get_footer(); ?>
