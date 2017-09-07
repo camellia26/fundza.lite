8768e1261dc48fe519dcfdcfe2192355a9aa5c9d
Yoshiaki NAGASAWA <ykk.26camellia@gmail.com>
2017-09-07 10:26:20 +0200

    add postrate function

--- single.php
+++ single.php
@@ -93,8 +93,9 @@
         </a>
     </p>
 
-    <!-- Comment Template -->
+    <!-- Rate and Comment Templates. Only shown on the last page -->
     <?php if( count( $pages ) == 1 || count( $pages ) == get_query_var( 'page' )){ ?>
+        <div class="ratings"><?php if(function_exists('the_ratings')) { the_ratings(); } ?></div>
         <?php comments_template(); ?>
     <?php } ?>
 
